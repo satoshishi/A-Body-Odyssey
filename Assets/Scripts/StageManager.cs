0aa487c5666df3e8e36873a25386518358565b17
satoshishi <satoshi1327@gmail.com>
2017-03-10 02:36:21 +0900

    refulection probeの適応 + materialの調整

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -63,7 +63,7 @@ public class StageManager : MonoBehaviour
 
         InitStages();
  //  ChangeStage(SCENE_TYPE.NOON);
-        ChangeStage(SCENE_TYPE.I);
+        ChangeStage(SCENE_TYPE.EAT);
     }
 
     void Update()
